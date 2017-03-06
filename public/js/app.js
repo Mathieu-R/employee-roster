5ee04d1e330c9d1f8354ac6efdd14dcdfe0a649b
Joshua Azemoh <joshazemoh@gmail.com>
2017-03-06 06:15:41 +0100

    gghhg

--- public/js/app.js
+++ public/js/app.js
@@ -52,12 +52,28 @@ app.factory('EmployeeFactory', ['$http', function ($http) {
 	}
 }]);
 
-app.controller('employeesController', ['$scope', '$state', 'EmployeeFactory', function ($scope, $state, EmployeeFactory) {
+app.service('formService', [ function () {
+	var state = {};
+	var _addshow = false;
+
+	state.addshow = function () {
+		return _addshow;
+	}
+
+	state.set = {
+		addshow(value) {
+			_addshow = value;
+		}
+	}
+}]);
+
+app.controller('employeesController', ['$scope', '$state', 'EmployeeFactory', 'formService', function ($scope, $state, EmployeeFactory, formService) {
 	$scope.editingrow = -1;
 	$scope.employeenumber;
   $scope.selrows = [];
 	$scope.employees = [];
 	$scope.page = 1;
+	$scope.addshow = false;
 	$scope.employeeCount = 0;
 
 	function fetchData(page) {
@@ -144,9 +160,18 @@ app.controller('employeesController', ['$scope', '$state', 'EmployeeFactory', fu
 	};
 
 	fetchData();
+
+	$scope.$on('update', (e, msg) => {
+		fetchData();
+	});
+
+	$scope.$on('editing', (e, msg) => {
+		$scope.addshow = msg;
+	});
 }]);
 
-app.controller('formController', ['$scope', '$state', 'EmployeeFactory', '$stateParams', function ($scope, $state, EmployeeFactory, $stateParams) {
+app.controller('formController', ['$scope', '$rootScope', '$state', 'EmployeeFactory', '$stateParams',
+	function ($scope, $rootScope, $state, EmployeeFactory, $stateParams) {
   $scope.formModel = {};
 	$scope.editflag = 0;
 	$scope.designations = [{
@@ -183,6 +208,8 @@ app.controller('formController', ['$scope', '$state', 'EmployeeFactory', '$state
 	];
 
 	if ($stateParams.number) {
+		$scope.editflag = 1;
+
 		EmployeeFactory.getEmployee($stateParams.number)
 			.then((response) => {
 				var data = response.data;
@@ -205,8 +232,10 @@ app.controller('formController', ['$scope', '$state', 'EmployeeFactory', '$state
 				$scope.status = 'Employee not found: ' + error.message;
 			})
 	} else {
-		
+		$scope.editflag = 0;
 	}
+
+	$rootScope.$broadcast('editing', true);
 
   $scope.onSubmit = function () {
 		// Number is invalid
@@ -228,7 +257,7 @@ app.controller('formController', ['$scope', '$state', 'EmployeeFactory', '$state
 			salary: $scope.formModel.salary
 		};
 
-		if ($scope.editflag == 0) {
+		if ($scope.editflag == 1) {
 			//insert.
 			EmployeeFactory.updateEmployee(employeeObj.number, employeeObj)
         .then(function (response) {}, function (error) {
@@ -242,6 +271,7 @@ app.controller('formController', ['$scope', '$state', 'EmployeeFactory', '$state
         });
 		}
 
+		$rootScope.$broadcast('update', null);
 		$state.go('employees');
   };
 
@@ -266,288 +296,4 @@ app.controller('formController', ['$scope', '$state', 'EmployeeFactory', '$state
 			}
 		}
 	}
-	
-}]);
-
-app.controller('EmployeeRosterCtrl', ['$scope', 'EmployeeFactory', function ($scope, EmployeeFactory) {
-  $scope.formMode = true; //true : insert,  false : edit
-	$scope.addshow = false;
-	$scope.editingrow = -1;
-	$scope.employeenumber;
-  $scope.selrows = [];
-	$scope.employees = [];
-  $scope.formModel = {};
-	$scope.page = 1;
-	$scope.employeeCount = 0;
-
-  function fetchData(page) {
-    EmployeeFactory.getEmployees(page)
-      .then(function (response) {
-        $scope.employees = response.data.employees;
-				$scope.employeeCount = response.data.count;
-      }, function (error) {
-          $scope.status = 'Unable to load customer data: ' + error.message;
-      });
-  }
-
-  function init() {
-    fetchData();
-    initFormModel();
-    initTableModel();
-  }
-
-	$scope.pageChanged = function (page) {
-		fetchData(page);
-	}
-  
-  function initFormModel() {
-    $scope.formModel = {
-      firstName: '',
-      lastName: '',
-      middleName: '',
-      age: '',
-      designation: '',
-      salary: '',
-      number: ''
-    };
-
-    $('input,select').removeClass('ng-dirty ng-touched ng-invalid ng-invalid-required');
-  };
-
-  function initTableModel() {
-		$scope.duplicated = false;
-		//$scope.addshow = false;
-		$scope.selrows = [];
-
-		$('table tr').removeClass('selected');
-	};
-
-  $scope.designations = [{
-			name: 'Consultant',
-			min: 30000,
-			max: 35000,
-			value: 30000
-		}, {
-			name: 'Senior Consultant',
-			min: 36000,
-			max: 40000,
-			value: 36000
-		}, {
-			name: 'Lead',
-			min: 41000,
-			max: 45000,
-			value: 41000
-		}, {
-			name: 'Assistant Manager',
-			min: 46000,
-			max: 50000,
-			value: 460004
-		}, {
-			name: 'Manager',
-			min: 51000,
-			max: 55000,
-			value: 510004
-		}, {
-			name: 'Senior Manager',
-			min: 56000,
-			max: 80000,
-			value: 560004
-		}
-	];
-
-	$scope.onCancel = function () {
-		initFormModel();
-		initTableModel();
-		$scope.addshow = false;
-	};
-
-  $scope.onSubmit = function () {
-		// Number is invalid
-		if ($('#submita').attr('disabled') == 'disabled')
-			return;
-
-		if ($scope.duplicated) {
-			$scope.formModel.number = '';
-			return;
-		}
-
-		var employeeObj = {
-			number: $scope.formModel.number,
-			firstName: $scope.formModel.firstName,
-			lastName: $scope.formModel.lastName,
-			middleName: $scope.formModel.middleName,
-			age: $scope.formModel.age,
-			designation: $scope.formModel.designation.name,
-			salary: $scope.formModel.salary
-		};
-
-		if ($scope.editflag == 0) {
-			//insert.
-			EmployeeFactory.updateEmployee(employeeObj.number, employeeObj)
-        .then(function (response) {}, function (error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
-        });
-		} else {
-			//push.
-			EmployeeFactory.createEmployee(employeeObj)
-        .then(function (response) {}, function (error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
-        });
-		}
-
-		// Clearing the form fields
-		initFormModel();
-
-		$scope.formMode = false;
-
-		initTableModel();
-		$scope.addshow = false;
-    fetchData();
-  };
-
-	$scope.getMinSalaryfunction = function () {
-		return $('span.min').html();
-	};
-
-	$scope.getMaxSalaryfunction = function () {
-		return $('span.max').html();
-	};
-
-  $scope.add = function () {
-		if ($scope.addshow && !$scope.formMode)
-			return;
-
-		$scope.employeenumber = -1;
-
-		$scope.selrows = [];
-		$('table tr').removeClass('selected');
-
-		initFormModel();
-
-		$scope.formMode = true;
-		$scope.addshow = true;
-		$scope.editflag = 1;
-	};
-
-	$scope.delete = function () {
-		//sort the No(order in table) array of checked rows.
-		$scope.selrows.sort();
-
-		for (i = $scope.selrows.length - 1; i >= 0; i--) {
-      $scope.employeenumber = $scope.selrows[i];
-      var emp = $scope.employees.find((employee) => employee.number == $scope.employeenumber);
-
-      EmployeeFactory.deleteEmployee(emp.number)
-        .then(function (response) {
-          if ($scope.editingrow === emp.number) {
-            $scope.addshow = false;
-          }
-          
-          fetchData();
-        }, function (error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
-        });
-		}
-		//disable the delete button.
-		initTableModel();
-
-		$scope.formMode = true;
-		$scope.$apply();
-	};
-
-	$scope.edit = function () {
-		if ($scope.addshow || $scope.selrows.length != 1)
-			return;
-
-		$scope.addshow = true;
-
-		$scope.employeenumber = $scope.selrows[0];
-    $scope.oldemployee = $scope.employees.find((employee) => employee.number == $scope.employeenumber);
-
-		var data = $scope.oldemployee;
-
-		$scope.editingrow = $scope.selrows[0];
-
-		$scope.formModel.number = eval(data.number);
-		$scope.formModel.firstName = data.firstName;
-		$scope.formModel.lastName = data.lastName;
-		$scope.formModel.middleName = data.middleName;
-		$scope.formModel.age = eval(data.age);
-
-		for (i = 0; i < $scope.designations.length; i++) {
-
-			if ($scope.designations[i].name == data.designation) {
-				$scope.formModel.designation = $scope.designations[i];
-				$scope.formModel.salary = eval(data.salary);
-			}
-		}
-
-		$scope.formMode = false;
-		$scope.editflag = 0;
-	};
-
-  $scope.onNumberChange = function () {
-		//check duplication.\
-		if ($scope.employees == null || $scope.formModel.number == '') return;
-
-		for (i = 0; i < $scope.employees.length; i++) {
-
-			if ($scope.employees[i].number == $scope.formModel.number) {
-				if ($scope.editflag == 1) {
-					//when add.
-					$scope.duplicated = true;
-					return;
-				} else {
-
-					//when edit.
-					if ($scope.employeenumber != i) {
-						$scope.duplicated = true;
-						return;
-					}
-				}
-			}
-		}
-
-		$scope.duplicated = false;
-	};
-
-	$scope.openmodal = function (event) {
-		if ($scope.selrows.length < 1)
-			return;
-		BootstrapDialog.show({
-			title: 'Delete',
-			message: 'Are you sure you want to delete  ' + ($scope.selrows.length >= 2 ? ' these ' + $scope.selrows.length + ' entries? ' : ' this entry? '),
-			closable: false,
-			buttons: [{
-				label: '<i class="fa fa-trash"></i>&nbsp;&nbsp;Delete',
-				cssClass: 'btn-lg btn-green delete-button-delete',
-				action: function (dialogRef) {
-					$scope.delete();
-					dialogRef.close();
-				}
-			}, {
-				label: '<i class="fa fa-times"></i>&nbsp;&nbsp;Cancel',
-				cssClass: 'btn-lg btn-gray delete-button-cancel',
-				action: function (dialogRef) {
-					dialogRef.close();
-				}
-			}]
-		});
-	};
-
-	$scope.selectRow = function (event, number) {
-    $(event.currentTarget).toggleClass('selected');
-    var index = $scope.selrows.indexOf(number);
-    
-    if (index === -1)
-      $scope.selrows.push(number);
-    else
-      $scope.selrows.splice(index, 1);
-	};
-
-  init();
-
-	$('a').mouseup(function () {
-		$(this).blur();
-	});
 }]);
